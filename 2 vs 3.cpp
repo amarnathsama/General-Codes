fc09c3517c612d2744a2f39389485c3f7ed19139
Shraeyas <shraeyassingh@gmail.com>
2018-01-18 12:22:44 +0530

    Segment Tree

--- 2 vs 3.cpp
+++ 2 vs 3.cpp
@@ -2,11 +2,44 @@
 
 using namespace std;
 
+int pow(int x, int y)
+{
+  int res = 1;
+  
+  while(y)
+  {
+    if(y & 1)
+      res = (res * x) % 3;
+      
+	x = (x * x) % 3;
+    y >>= 1;
+  }
+  
+  return res;
+}
+
+int combine(int a, int b, int la, int lb)
+{
+	int ans;
+	
+	if(la == 0 || a == -1)
+	return b % 3;
+	
+	if(lb == 0 || b == -1)
+	return a % 3;
+	
+	ans = ((a * pow(2, lb)) % 3 + b % 3) % 3;
+	
+	return ans;
+}
+
+
 void build(string &s, int lo, int hi, int *segtree, int pos)
 {
 	if(lo == hi)
 	{
 		segtree[pos] = (s[lo] == '1' ? 1 : 0);
+		
 		return;
 	}
 	
@@ -15,19 +48,19 @@ void build(string &s, int lo, int hi, int *segtree, int pos)
 	build(s, lo, mid, segtree, 2 * pos + 1);
 	build(s, mid + 1, hi, segtree, 2 * pos + 2);
 	
-	segtree[pos] = (segtree[2 * pos + 1] + segtree[2 * pos + 2]);
+	segtree[pos] = combine(segtree[2 * pos + 1], segtree[2 * pos + 2], mid - lo + 1, hi - mid);
 }
 
 int query(int lo, int hi, int *segtree, int pos, int qlo, int qhi)
 {
 	if(qlo <= lo && qhi >= hi)
 	{
-		return segtree[pos] % 3;
+		return segtree[pos];
 	}
 	
 	if(qlo > hi || qhi < lo)
-	{
-		return 0;
+	{		
+		return -1;
 	}
 	
 	int mid = (lo + hi)/2;
@@ -35,7 +68,32 @@ int query(int lo, int hi, int *segtree, int pos, int qlo, int qhi)
 	int l = query(lo, mid, segtree, 2 * pos + 1, qlo, qhi);
 	int r = query(mid + 1, hi, segtree, 2 * pos + 2, qlo, qhi);
 	
-	return (l + r) % 3;
+	return combine(l, r, mid - lo + 1, hi - mid);
+}
+
+void update(string a, int lo, int hi, int *segtree, int pos, int ind)
+{
+	if(lo == hi)
+	{
+		a[lo] = '1';
+		segtree[pos] = 1;
+		
+		return;
+	}
+	
+	int mid = (lo + hi)/2;
+	
+	if(mid <= ind)
+	{
+		update(a, lo, mid, segtree, 2 * pos + 1, ind);
+	}
+	
+	else
+	{
+		update(a, mid + 1, hi, segtree, 2 * pos + 2, ind);
+	}
+	
+	segtree[pos] = combine(segtree[2 * pos + 1], segtree[2 * pos + 2], mid - lo + 1, hi - mid);
 }
 
 int main()
@@ -57,11 +115,18 @@ int main()
 	while(q--)
 	{
 		int x, qlo, qhi;
-		cin >> x >> qlo >> qhi;
+		cin >> x;
 		
 		if(!x)
 		{
-			cout << query(0, n - 1, segtree, 0, qlo, qhi) << endl;
+			cin >> qlo >> qhi;
+			cout << query(0, n - 1, segtree, 0, qlo, qhi) % 3 << endl;
+		}
+		
+		else
+		{
+			cin >> qlo;
+			update(s, 0, n - 1, segtree, 0, qlo);
 		}
 	}
 }

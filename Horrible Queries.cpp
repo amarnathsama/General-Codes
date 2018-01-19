ef7a2c66f992ceb67846cc28b1aa11b575a12dac
Shraeyas <shraeyassingh@gmail.com>
2018-01-19 13:21:33 +0530

    Lazy PRopagation

--- Horrible Queries.cpp
+++ Horrible Queries.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 struct node
 {
-  int sum;
-  int lazy;
+  long sum;
+  long lazy;
 };
 
-void build(int lo, int hi, node *segtree, int pos)
+void build(long lo, long hi, node *segtree, long pos)
 {
   if(lo == hi)
   {
@@ -18,7 +18,7 @@ void build(int lo, int hi, node *segtree, int pos)
     return;
   }
   
-  int mid = (lo + hi)/2;
+  long mid = (lo + hi)/2;
   
   build(lo, mid, segtree, 2 * pos + 1);
   build(mid + 1, hi, segtree, 2 * pos + 2);
@@ -27,7 +27,7 @@ void build(int lo, int hi, node *segtree, int pos)
   segtree[pos].lazy = 0;
 }
 
-void update(int lo, int hi, node *segtree, int pos, int v, int qlo, int qhi)
+void update(long lo, long hi, node *segtree, long pos, long v, long qlo, long qhi)
 {
     if(qlo <= lo && qhi >= hi)
     {
@@ -40,13 +40,13 @@ void update(int lo, int hi, node *segtree, int pos, int v, int qlo, int qhi)
 		return;
 	}
 	
-	int mid = (lo + hi)/2;
+	long mid = (lo + hi)/2;
 	
 	update(lo, mid, segtree, 2 * pos + 1, v, qlo, qhi);
 	update(mid + 1, hi, segtree, 2 * pos + 2, v, qlo, qhi);
 }
 
-int query(int lo, int hi, node *segtree, int pos, int qlo, int qhi)
+long query(long lo, long hi, node *segtree, long pos, long qlo, long qhi)
 {
 	if(qlo <= lo && qhi >= hi)
 	{
@@ -65,10 +65,15 @@ int query(int lo, int hi, node *segtree, int pos, int qlo, int qhi)
 		return 0;
 	}
 	
-	int mid = (lo + hi)/2;
+	long mid = (lo + hi)/2;
 	
-	int x = query(lo, mid, segtree, 2 * pos + 1, qlo, qhi);
-	int y = query(mid + 1, hi, segtree, 2 * pos + 2, qlo, qhi);
+	segtree[pos].sum += segtree[pos].lazy;    
+	segtree[2 * pos + 1].lazy += segtree[pos].lazy;
+	segtree[2 * pos + 2].lazy += segtree[pos].lazy;
+	segtree[pos].lazy = 0;
+	
+	long x = query(lo, mid, segtree, 2 * pos + 1, qlo, qhi);
+	long y = query(mid + 1, hi, segtree, 2 * pos + 2, qlo, qhi);
 	
 	return x + y;
 }
@@ -77,17 +82,19 @@ int main()
 {
   freopen("in.in", "r", stdin);
  
-  int t;
+  node *segtree = new node [4000001];
+ 
+  long t;
   cin >> t;
   
   while(t--)
   {
-    int n, c;
+    long n, c;
     cin >> n >> c;
       
-    node *segtree = new node [4 * n];
     
-    build(0, n - 1, segtree, 0);
+    
+   build(0, n - 1, segtree, 0);
     
     while(c--)
     {
@@ -96,7 +103,7 @@ int main()
       
       if(!c)
       {
-        int p, q, v;
+        long p, q, v;
         cin >> p >> q >> v;
         
         update(0, n - 1, segtree, 0, v, p - 1, q - 1);
@@ -104,11 +111,13 @@ int main()
       
       else
       {
-        int p, q;
+        long p, q;
         cin >> p >> q;
         
         cout << query(0, n - 1, segtree, 0, p - 1, q - 1) << endl;
       }
+      
+      
     }
   }
 }

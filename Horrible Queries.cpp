dfde09c04c912315442d0cc1fab23210f9689efd
Shraeyas <shraeyassingh@gmail.com>
2018-01-21 23:33:44 +0530

    Lazy Propagation

--- Horrible Queries.cpp
+++ Horrible Queries.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 struct node
 {
-  long sum;
-  long lazy;
+  long long sum, lazy;
 };
 
-void build(long lo, long hi, node *segtree, long pos)
+void build(long long lo, long long hi, node *segtree, long long pos)
 {
   if(lo == hi)
   {
@@ -18,7 +17,7 @@ void build(long lo, long hi, node *segtree, long pos)
     return;
   }
   
-  long mid = (lo + hi)/2;
+  long long mid = (lo + hi)/2;
   
   build(lo, mid, segtree, 2 * pos + 1);
   build(mid + 1, hi, segtree, 2 * pos + 2);
@@ -27,74 +26,95 @@ void build(long lo, long hi, node *segtree, long pos)
   segtree[pos].lazy = 0;
 }
 
-void update(long lo, long hi, node *segtree, long pos, long v, long qlo, long qhi)
+void update(long long lo, long long hi, node *segtree, long long pos, long long v, long long qlo, long long qhi)
 {
+	if(segtree[pos].lazy != 0)
+	{
+		segtree[pos].sum += segtree[pos].lazy * (hi - lo + 1);
+		    
+	    if(lo != hi)
+		{			
+			segtree[2 * pos + 1].lazy += segtree[pos].lazy;
+			segtree[2 * pos + 2].lazy += segtree[pos].lazy;
+		}
+		
+		segtree[pos].lazy = 0;	
+	}
+	
     if(qlo <= lo && qhi >= hi)
     {
-    	segtree[pos].lazy += v;
-        return;
+    	segtree[pos].sum += v * (hi - lo + 1);
+		
+		if(lo != hi)
+		{			
+			segtree[2 * pos + 1].lazy += v;
+			segtree[2 * pos + 2].lazy += v;
+		}
+		
+		return;
     }
-  
+
   	if(qlo > hi || qhi < lo)
 	{
 		return;
 	}
 	
-	long mid = (lo + hi)/2;
+	long long mid = (lo + hi)/2;
 	
 	update(lo, mid, segtree, 2 * pos + 1, v, qlo, qhi);
 	update(mid + 1, hi, segtree, 2 * pos + 2, v, qlo, qhi);
+	
+	segtree[pos].sum = segtree[2 * pos + 1].sum + segtree[2 * pos + 2].sum;
 }
 
-long query(long lo, long hi, node *segtree, long pos, long qlo, long qhi)
-{
-	if(qlo <= lo && qhi >= hi)
-	{
-		segtree[pos].sum += segtree[pos].lazy;
-        
-	    segtree[2 * pos + 1].lazy += segtree[pos].lazy;
-	    segtree[2 * pos + 2].lazy += segtree[pos].lazy;
-	    
-	    segtree[pos].lazy = 0;
-	    
-	    return segtree[pos].sum;
-	}
-	
+long long query(long long lo, long long hi, node *segtree, long long pos, long long qlo, long long qhi)
+{	
 	if(qlo > hi || qhi < lo)
 	{
 		return 0;
 	}
 	
-	long mid = (lo + hi)/2;
+	if(segtree[pos].lazy != 0)
+	{
+		segtree[pos].sum += segtree[pos].lazy * (hi - lo + 1);
+	    
+	    if(lo != hi)
+		{			
+			segtree[2 * pos + 1].lazy += segtree[pos].lazy;
+			segtree[2 * pos + 2].lazy += segtree[pos].lazy;
+		}
+		
+		segtree[pos].lazy = 0;	
+	}
 	
-	segtree[pos].sum += segtree[pos].lazy;    
-	segtree[2 * pos + 1].lazy += segtree[pos].lazy;
-	segtree[2 * pos + 2].lazy += segtree[pos].lazy;
-	segtree[pos].lazy = 0;
+	if(qlo <= lo && qhi >= hi)
+	{    
+	    return segtree[pos].sum;
+	}
 	
-	long x = query(lo, mid, segtree, 2 * pos + 1, qlo, qhi);
-	long y = query(mid + 1, hi, segtree, 2 * pos + 2, qlo, qhi);
+	long long mid = (lo + hi)/2;
+	
+	long long x = query(lo, mid, segtree, 2 * pos + 1, qlo, qhi);
+	long long y = query(mid + 1, hi, segtree, 2 * pos + 2, qlo, qhi);
 	
 	return x + y;
 }
 
 int main() 
 {
-  freopen("in.in", "r", stdin);
+  //freopen("in.in", "r", stdin);
  
-  node *segtree = new node [4000001];
- 
-  long t;
+  long long t;
   cin >> t;
   
   while(t--)
   {
-    long n, c;
+    long long n, c;
     cin >> n >> c;
       
+    node *segtree = new node [4000000];
     
-    
-   build(0, n - 1, segtree, 0);
+    build(0, n - 1, segtree, 0);
     
     while(c--)
     {
@@ -103,7 +123,7 @@ int main()
       
       if(!c)
       {
-        long p, q, v;
+        long long p, q, v;
         cin >> p >> q >> v;
         
         update(0, n - 1, segtree, 0, v, p - 1, q - 1);
@@ -111,13 +131,11 @@ int main()
       
       else
       {
-        long p, q;
+        long long p, q;
         cin >> p >> q;
         
         cout << query(0, n - 1, segtree, 0, p - 1, q - 1) << endl;
       }
-      
-      
     }
   }
 }

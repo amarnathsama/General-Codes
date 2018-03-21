294ab1a43084cfd74e576c940714948e8ff375ed
Shreyas Singh <Shraeyas@users.noreply.github.com>
2018-03-21 10:26:04 +0530

    Add files via upload

---
+++ remove null productions.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <tr1/unordered_map>
+
+using namespace std;
+
+vector <string> v;
+vector <char> name;
+tr1 :: unordered_map <char, bool> e;
+
+void solve(string s, int i, string ans)
+{
+	if(i == s.length())
+	{
+		cout << ans << " | ";
+		return;
+	}
+	
+	if(e[s[i]])
+	{
+		solve(s, i + 1, ans + s[i]);
+		solve(s, i + 1, ans);
+	}
+	
+	else
+	{
+		if(s[i] != '|' && s[i] != '~')
+		solve(s, i + 1, ans + s[i]);
+		
+		else
+		solve(s, i + 1, ans);
+	}
+}
+
+int main()
+{
+	freopen("in.in", "r", stdin);
+	
+	//cout << "Input Productions" << endl;
+	string s;
+	
+	while(getline(cin, s), s != "0")
+	{
+		string st = "";
+		
+		for(int i = 2 ; i < s.length() ; i++)
+		{
+			st += s[i];
+		}
+		
+		name.push_back(s[0]);
+		v.push_back(st);
+	}
+	
+	//cout << "Null Productions" << endl;
+	
+	while(getline(cin, s), s != "0")
+	{
+		e[s[0]] = 1;
+	}
+	
+	for(int i = 0 ; i < v.size() ; i++)
+	{
+		cout << name[i] << " -> ";
+		solve(v[i], 0, "");
+		cout << endl;
+	}
+}
